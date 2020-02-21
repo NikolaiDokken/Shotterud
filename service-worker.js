7987e0c4ad9dbeb862c8f6abcc62ec0cae551be8
NikolaiDokken <49611368+NikolaiDokken@users.noreply.github.com>
2020-02-21 00:38:41 +0000

    Deploying to gh-pages from  - bd8c03edf5a30947cffce425e2073f2985a69444 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9a3c8ca637e02f9513cce3e8cf27c72b.js"
+  "/Shotterud/precache-manifest.b9dd1aa5f8a86ba0948532084efd74b6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Shotterud/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
